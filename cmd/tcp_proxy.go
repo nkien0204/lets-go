e51ef584d2293518e733ae21a2032c0721da4640
kiennt <kiennt@edsolabs.com>
2022-01-14 15:47:10 +0700

    update log

---
+++ cmd/tcp_proxy.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/nkien0204/projectTemplate/configs"
+	"github.com/nkien0204/projectTemplate/internal/log"
+	"github.com/nkien0204/projectTemplate/internal/network/tcp_handler/tcp_proxy"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var runProxyCmd = &cobra.Command{
+	Use:   "proxy",
+	Short: "start proxy server",
+	Run:   runProxy,
+}
+
+func init() {
+	serveCmd.AddCommand(runProxyCmd)
+}
+
+func runProxy(cmd *cobra.Command, args []string) {
+	logger := log.Logger()
+	configs.Config = configs.InitConfigs()
+
+	if err := tcp_proxy.EstablishProxy([]string{"0.0.0.0:9001"}); err != nil {
+		logger.Error("establish proxy failed", zap.Error(err))
+	}
+
+	// graceful shutdown
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+	log.Logger().Warn("shutdown app")
+}
---
+++ internal/network/tcp_handler/tcp_proxy/proxy.go
@@ -0,0 +1,62 @@
+package tcp_proxy
+
+import (
+	"github.com/nkien0204/projectTemplate/configs"
+	"github.com/nkien0204/projectTemplate/internal/log"
+	"go.uber.org/zap"
+	"io"
+	"net"
+)
+
+func EstablishProxy(dstAddresses []string) error {
+	logger := log.Logger()
+	proxyServer, err := net.Listen("tcp", configs.Config.TcpProxyServer.ProxyAddress)
+	if err != nil {
+		logger.Error("listen proxyServer failed", zap.Error(err))
+		return err
+	}
+
+	go serveProxy(proxyServer, dstAddresses)
+
+	return nil
+}
+
+func serveProxy(proxyServer net.Listener, dstAddresses []string) {
+	logger := log.Logger()
+	for {
+		conn, err := proxyServer.Accept()
+		if err != nil {
+			logger.Error("accept proxy conn failed", zap.Error(err))
+			return
+		}
+
+		go func(from net.Conn) {
+			defer from.Close()
+
+			for _, dstAddr := range dstAddresses {
+				to, err := net.Dial("tcp", dstAddr)
+				if err != nil {
+					logger.Error("proxy dial failed", zap.Error(err))
+					return
+				}
+				defer to.Close()
+
+				err = proxy(from, to)
+				if err != nil && err != io.EOF {
+					logger.Error("error while serving proxy", zap.Error(err))
+				}
+			}
+		}(conn)
+	}
+}
+
+func proxy(from io.Reader, to io.Writer) error {
+	fromWriter, fromIsWriter := from.(io.Writer)
+	toReader, toIsReader := to.(io.Reader)
+
+	if toIsReader && fromIsWriter {
+		go func() { io.Copy(fromWriter, toReader) }()
+	}
+	_, err := io.Copy(to, from)
+	return err
+}
