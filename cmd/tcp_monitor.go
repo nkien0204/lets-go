6c99d64c3b3cd35daf01fe20d3df330e1e75bc45
Kien <nkien0204@gmail.com>
2022-01-15 16:56:31 +0700

    update code

---
+++ cmd/tcp_monitor.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/nkien0204/projectTemplate/internal/network/tcp_handler/tcp_monitor"
+	"github.com/nkien0204/projectTemplate/internal/log"
+	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var runMonitorCmd = &cobra.Command{
+	Use:   "monitor",
+	Short: "start tcp monitor",
+	Run:   runMonitor,
+}
+
+func init() {
+	serveCmd.AddCommand(runMonitorCmd)
+}
+
+func runMonitor(cmd *cobra.Command, args []string) {
+	tcp_monitor.ExampleMonitor()
+
+	// graceful shutdown
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+	log.Logger().Warn("shutdown app")
+}
