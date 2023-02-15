61e8cc945bf958e7b57c37f5272ab7a311864ea2
kien Nguyen <nkien0204@gmail.com>
2023-02-15 10:48:53 +0700

    done for mail client

---
+++ internal/network/mailclient/test/client_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/nkien0204/lets-go/internal/network/mailclient"
+)
+
+func TestMailClient(t *testing.T) {
+	client := mailclient.NewMailClient("smtp.example.com", "smtp.example.com:587", "from@example.com", "password")
+	err := client.SendMail("testing", "hello", "to@example.com", nil, []string{""}); if err != nil {
+		t.Error(err)
+	}
+}
